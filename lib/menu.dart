60fe1ddaf5ed7da5d2cfa5bcc0dc720b63baeed5
Hiroshi <habibil.pplg1@gmailu.com>
2026-09-10 12:06:23 +0700

    buat class menu

---
+++ lib/menu.dart
@@ -0,0 +1,15 @@
+class Menu {
+  final String namamenu;
+  final String kategori;
+  final int harga;
+  final bool tersedia;
+  final int stokporsi;
+
+  Menu({
+    required this.namamenu,
+    required this.kategori,
+    required this.harga,
+    required this.tersedia,
+    required this.stokporsi,
+  });
+}
